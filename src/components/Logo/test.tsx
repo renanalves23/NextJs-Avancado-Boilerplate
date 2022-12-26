8ad6500b22683779f146c8880c0406b215b5ffc3
renanalves23 <renanalves23@outlook.com>
2022-12-26 08:26:14 -0300

    renderWithTheme helper method

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
+  it('should render the white logo by default', () => {
     const { container } = render(<Logo />)
 
     expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
